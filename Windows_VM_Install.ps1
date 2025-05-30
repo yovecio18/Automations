c243167c6419a1459d28e4c32f881029b4efc2e4
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2025-05-30 11:10:28 +0200

    Create Windows_VM_Install.ps1

---
+++ Windows_VM_Install.ps1
@@ -0,0 +1,22 @@
+echo "Creating Exclusion/Tools folder"
+mkdir C:\Exclusion
+mkdir C:\Exclusion\Tools
+
+echo "Adding folder exclusion in Defender"
+Add-MpPreference -ExclusionPath C:\Exclusion\
+
+echo "Setting Swedish time zone"
+Set-TimeZone -Id "Central Europe Standard Time"
+
+echo "Intalling Spice Agent"
+cd C:\Exclusion\Tools
+wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win-guest-tools.exe -outfile virtio-win-guest-tools.exe
+msiexec /qn /i virtio-win-guest-tools.exe
+
+echo "Fetching & executing debloater tool"
+cd C:\Exclusion\Tools
+wget https://github.com/Raphire/Win11Debloat/releases/download/2025.05.26/Get.ps1 -outfile Get.ps1
+Set-ExecutionPolicy Bypass -Force
+powershell.exe ./Get.ps1
+Set-ExecutionPolicy Restricted -Force
+
