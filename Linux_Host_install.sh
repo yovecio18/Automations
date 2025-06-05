2e147cca7d4a1f9649f6819de6b1a8ece79ec344
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2025-06-05 14:13:11 +0200

    Update Linux_Host_install.sh

--- Linux_Host_install.sh
+++ Linux_Host_install.sh
@@ -69,6 +69,11 @@ cd gnome-terminal
 chmod +x ./install.sh
 ./install.sh
 
+echo "Installing LUKS tools"
+#Reference: https://funzt.info/posts/debian-using-yubikey-to-unlock-luks-encrypted-disks/
+sudo apt install -y yubikey-personalization yubikey-luks
+
+
 #echo "Installing Debian's Nvidia and Hashcat"
 #sudo apt install -y nvidia-driver nvdia-smi nvidia-cuda-toolkit hashcat
 echo "Installing Ubuntu's Nvidia and Hashcat"
