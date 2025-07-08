dd06e81f8fd09d620eca3dd95a0df22b1df780d7
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2025-07-08 09:23:37 +0200

    Update Linux_Host_install.sh

--- Linux_Host_install.sh
+++ Linux_Host_install.sh
@@ -84,6 +84,6 @@ sudo ubuntu-drivers install
 echo "Signing VMware Worksstation"
 cd /opt
 sudo openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=VMware/"
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(/sbin/modinfo -n vmmon)
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(/sbin/modinfo -n vmnet)
+sudo /usr/src/linux-headers-`uname -r`scripts/sign-file sha256 ./MOK.priv ./MOK.der $(/sbin/modinfo -n vmmon)
+sudo /usr/src/linux-headers-`uname -r`scripts/sign-file sha256 ./MOK.priv ./MOK.der $(/sbin/modinfo -n vmnet)
 sudo mokutil --import MOK.der
