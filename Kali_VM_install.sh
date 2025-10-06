521ae547ed78b0f077a4b5061d17ab0d4ac1e339
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2025-10-06 15:03:44 +0200

    Update Kali_VM_install.sh

--- Kali_VM_install.sh
+++ Kali_VM_install.sh
@@ -169,6 +169,7 @@ sudo apt install -y dbeaver
 
 echo "Cloning some extra repositories"
 cd /home/user/Downloads/Tools
+wget https://github.com/Krptyk/Cpp-Reverse-Shell/releases/download/Release/CppRevShell-PS.exe
 wget https://github.com/yovecio18/Automations/raw/refs/heads/main/Certify.exe
 wget https://raw.githubusercontent.com/juliourena/plaintext/refs/heads/master/Powershell/Get-GPOEnumeration.ps1
 wget https://github.com/yovecio18/Automations/raw/refs/heads/main/SharpWSUS.exe
